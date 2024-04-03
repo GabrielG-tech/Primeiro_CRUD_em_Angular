9ff5be83ee2f872a5b5c08186ac241d708a12916
Gabriel Gomes <gabriel@mpsouza.com.br>
2024-04-03 17:15:13 -0300

    .

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
-// import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
